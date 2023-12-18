b0dc803909556c8fbf11383b78338ae38a1d0fad
mhamad raad <hamaraad883@gmail.com>
2023-12-18 21:06:16 +0300

    add the samples for checkings

---
+++ 3- Regular Expression/regex.js
@@ -0,0 +1,19 @@
+// write the string samples.
+
+const sampleDigit = '123';
+const sampleLetter = 'abc';
+const sampleCapitalLetter = 'ABC';
+const sampleSmallLetter = 'xyz';
+const sampleDollarSign = '$';
+const sampleSpecialCharacter = '!@#$';
+const sampleWord = 'example';
+const sampleEmail = 'test@example.com';
+const sampleURL = 'http://www.example.com';
+const samplePhoneNumber = '123-456-7890';
+const sampleDate = '2023-01-01';
+const sampleIPAddress = '192.168.0.1';
+const sampleHTMLTag = '<div>Hello</div>';
+const sampleWhiteSpace = ' ';
+const samplePrice = '$19.99';
+const sampleWeight = '2.5 kg';
+const sampleDimension = '10.5 inches';
