434642eedf429574d0da86f6abee8de55e9a2e4b
joker69216 <hamaraad883@gmail.com>
2023-12-24 15:06:56 +0300

    add final touches

--- 4-backend-express/routes/task4.router.js
+++ 4-backend-express/routes/task4.router.js
@@ -4,13 +4,11 @@ const { sendTaskFour } = require('./task4.controller')
 
 const router = express.Router();
 
-// Middleware to log the time
 router.use('/task4', function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   next();
 });
 
-// Route to serve the HTML file
 router.get('/task4', sendTaskFour);
 
 module.exports = router;
