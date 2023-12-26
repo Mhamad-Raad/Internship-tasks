5a637ac2f543bec633561e8c99492359744ad3f1
mhamad raad <hamaraad883@gmail.com>
2023-12-26 14:01:27 +0300

    create table in database

--- 5-DataBase-API/server.js
+++ 5-DataBase-API/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('database.db');
 
-const task4Route = require('./routes/task4.router')
+const task4Route = require('./routes/task4.router');
 
 const port = 3000;
 
@@ -13,7 +15,7 @@ app.get('/', (req, res) => {
 //for the css
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.get('/task4', task4Route) 
+app.get('/task4', task4Route);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
