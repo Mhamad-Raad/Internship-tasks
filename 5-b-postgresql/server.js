3f77d13047878b485b6b6de2b6b13d04fbea642b
mhamad raad <hamaraad883@gmail.com>
2023-12-26 15:06:40 +0300

    update the code to postgresql

--- 5-b-postgresql/server.js
+++ 5-b-postgresql/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { Pool } = require('pg');
 
-const task4Route = require('./routes/task4.router')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'task5',
+  password: '1234',
+  port: 5432, // Default PostgreSQL port
+});
+
+const task4Route = require('./routes/task4.router');
 
 const port = 3000;
 
@@ -13,10 +22,41 @@ app.get('/', (req, res) => {
 //for the css
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.get('/task4', task4Route) 
+app.get('/task4', task4Route);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
+});
+
+app.use(express.json());
+
+app.get('/quotes', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM quotes');
+    console.log(rows);
+    res.send(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/quotes', async (req, res) => {
+  console.log(req.body);
+
+  const { author, quote } = req.body;
+
+  try {
+    await pool.query('INSERT INTO quotes (author, quote) VALUES ($1, $2)', [
+      author,
+      quote,
+    ]);
+
+    res.send('Done');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // Mhamad Raad
